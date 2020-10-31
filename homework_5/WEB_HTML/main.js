fc95c9d245202b9f06927b67c783b26e907febc5
esuder <60942175+esuder@users.noreply.github.com>
2020-10-30 21:12:58 -0400

    Add to Cart pop-up

--- homework_5/WEB_HTML/main.js
+++ homework_5/WEB_HTML/main.js
@@ -1,3 +1,26 @@
+//BACK BUTTON
+
 function goBack() {
   window.history.back();
+}
+
+//ADD TO CART POPUP
+
+function openPopup() {
+	modal = document.getElementById("myModal");
+	// When the user clicks on the button, open the modal
+	modal.style.display = "block";
+}
+
+function openSpan() {
+	modal = document.getElementById("myModal");
+	// When the user clicks on <span> (x), close the modal
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
